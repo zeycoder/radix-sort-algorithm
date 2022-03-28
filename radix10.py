ed039fe55ded8ea155ed7c4963bff9d99b1eab53
zeyneparslan55 <58356584+zeyneparslan55@users.noreply.github.com>
2022-03-28 11:35:56 +0300

    first commit

---
+++ radix10.py
@@ -0,0 +1,31 @@
+import time
+startTime = time.time()
+
+def radixSort(list):
+    maxLength = len(str(max(list)))
+    for i in range(maxLength):
+        buckets = [[] for _ in range(10)]
+        for j in list:
+            buckets[int(j/(10**i))%10].append(j)
+        list = [num for bucket in buckets for num in bucket]
+    return list
+
+def main():
+    file = open("10lukliste 7.txt", "r")
+    list = file.readlines()
+    maxLength = 3
+    
+    for i in range(len(list)):
+        list[i]=int(float(list[i])*(10**maxLength))
+    print(list)
+    sorted = radixSort(list)
+    
+    for i in range(len(sorted)):
+        sorted[i] = float(sorted[i]/(10**maxLength))
+        if sorted[i] == int(sorted[i]):
+            sorted[i] = int(sorted[i])
+    print(sorted)
+
+    print("--- %s saniye ---" % (time.time() - startTime))
+
+main()
